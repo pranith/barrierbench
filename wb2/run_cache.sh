3b1f0c2bf56a68573376d8772c9586c4bbeec14d
Pranith Kumar <bobby.prani@gmail.com>
2014-10-24 18:06:04 -0700

    update run script

--- wb2/run_cache.sh
+++ wb2/run_cache.sh
@@ -2,14 +2,18 @@
 
 # arg=$*
 
+rm -f nobarrier.txt barrier.txt
 make gen_define.exe
-for ((arg = 1; arg < 128; arg = arg * 2))
+for ((arg = 1; arg <= 1024; arg = arg * 2))
 do
     	./gen_define.exe $arg > defines.h
 	rm -f fitsincache.o fitsincache.exe
    	make >& /dev/null
-	./fitsincache.exe $arg 
+	./fitsincache.exe $arg | tee -a nobarrier.txt
 	rm -f fitsincache.o fitsincache.exe
 	make CPPFLAGS=-DUSE_BARRIER >& /dev/null
-	./fitsincache.exe $arg 
+	./fitsincache.exe $arg | tee -a barrier.txt
 done
+
+cat barrier.txt >> results.txt
+cat nobarrier.txt >> results.txt
