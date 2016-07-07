99c0b7a7465928b00f6dd7c8e34ec110c86b2f7d
Pranith Kumar <bobby.prani@gmail.com>
2016-07-07 12:51:32 -0400

    Increase cache line size and mem size

--- write_buf/common.h
+++ write_buf/common.h
@@ -1,7 +1,7 @@
 #define KB(x) ((x) << 10)
 #define MB(x) ((x) << 20)
 
-#define NUM_ACCESSES_PER_ITER 64
+#define NUM_ACCESSES_PER_ITER 40
 #define MEM_SIZE 128
-#define CACHE_LINE_SIZE 64
+#define CACHE_LINE_SIZE 128
 
